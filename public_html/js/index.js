8abb363af7b8a7c0318154145131bf509a3b17d6
Rhys Markham <markhamr1@southernct.edu>
2025-10-14 23:04:15 +0000

    Part 3

--- public_html/js/index.js
+++ public_html/js/index.js
@@ -2,12 +2,12 @@ window.onload = function() {
     // Inputs
     var nameInput = document.getElementById("name");
     var dayInput = document.getElementById("day");
-    var timeInput = document.getElementById("Time");
+    var timeInput = document.getElementById("time");
 
     // Buttons
-    var scheduleBtn = document.querySelector('button[type="schedule"]');
-    var cancelBtn = document.querySelector('button[type="cancel"]');
-    var checkBtn = document.querySelector('button[type="check"]');
+    var scheduleBtn = document.getElementById("scheduleBtn");
+    var cancelBtn = document.getElementById("cancelBtn");
+    var checkBtn = document.getElementById("checkBtn");
 
     // Results div
     var resultsDiv = document.getElementById("results");
@@ -16,24 +16,35 @@ window.onload = function() {
     scheduleBtn.onclick = function() {
         var url = "/schedule?name=" + nameInput.value + "&day=" + dayInput.value + "&time=" + timeInput.value;
         fetch(url)
-        .then(function(response){ return response.text(); })
-        .then(function(text){ resultsDiv.innerText = text; });
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(text) {
+            resultsDiv.innerText = text;
+        });
     };
 
     // Cancel button
     cancelBtn.onclick = function() {
         var url = "/cancel?name=" + nameInput.value + "&day=" + dayInput.value + "&time=" + timeInput.value;
         fetch(url)
-        .then(function(response){ return response.text(); })
-        .then(function(text){ resultsDiv.innerText = text; });
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(text) {
+            resultsDiv.innerText = text;
+        });
     };
 
     // Check button
     checkBtn.onclick = function() {
         var url = "/check?day=" + dayInput.value + "&time=" + timeInput.value;
         fetch(url)
-        .then(function(response){ return response.text(); })
-        .then(function(text){ resultsDiv.innerText = text; });
+        .then(function(response) {
+            return response.text();
+        })
+        .then(function(text) {
+            resultsDiv.innerText = text;
+        });
     };
 };
-
